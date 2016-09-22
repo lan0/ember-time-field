2a8db138bcd7662dc9392a265170bbffe89a5085
Michael Pitzer <mail@mpitzer.at>
2016-09-22 18:18:46 +0200

    Don't error on unhandled events in unfocused state

--- addon/states/manager.js
+++ addon/states/manager.js
@@ -1,5 +1,5 @@
+import Ember from 'ember';
 import StateManager from "ember-states/state-manager";
-
 import UnfocusedState from './unfocused';
 import FocusedState from './focused';
 
@@ -7,5 +7,11 @@ export default StateManager.extend({
   // enableLogging: true,
   initialState: 'unfocused',
   unfocused:    UnfocusedState,
-  focused:      FocusedState
+  focused:      FocusedState,
+
+  unhandledEvent(manager, event) {
+    if (this.get('currentState.name') !== 'unfocused') {
+      throw new Ember.Error(this.toString() + " could not respond to event " + event + " in state " + this.get('currentState.path') + ".");
+    }
+  },
 });
